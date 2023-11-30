5a20238d4868b0a25ab54ae55ee1bef039009e11
pphatt <vutienphat0809@gmail.com>
2023-11-30 21:59:35 +0700

    feat: add navigation bar

--- Assets/ts/search.ts
+++ Assets/ts/search.ts
@@ -1,1 +1,75 @@
-﻿
+﻿class Search {
+    private open: boolean = false;
+    private opacity: number = 0;
+
+    private searchWrapElement: HTMLDivElement;
+    private navBarBackground: HTMLDivElement;
+    private backdrop: HTMLDivElement;
+
+    constructor(elems: {
+        searchWrapElement: HTMLDivElement
+        navBarBackground: HTMLDivElement
+        backdrop: HTMLDivElement
+    }) {
+        const {searchWrapElement, navBarBackground, backdrop} = elems;
+
+        this.searchWrapElement = searchWrapElement
+        this.navBarBackground = navBarBackground
+        this.backdrop = backdrop;
+
+        this.setupEventListener();
+    }
+
+    setupEventListener() {
+        this.searchWrapElement.addEventListener("click", (e: MouseEvent) => {
+            e.stopPropagation();
+            
+            if (!this.open) {
+                this.open = true;
+            }
+
+            this.searchInputActive()
+        })
+
+        this.backdrop.addEventListener("click", (e: MouseEvent) => {
+            e.stopPropagation();
+
+            if (this.open) {
+                this.open = false;
+            }
+
+            this.searchInputActive()
+        })
+
+        window.addEventListener("scroll", () => {
+            if (window.scrollY <= 56) {
+                this.opacity = 0.0178571 * window.scrollY
+            } else {
+                this.opacity = 1
+            }
+        })
+
+        window.addEventListener("keydown", (e: KeyboardEvent) => {
+            if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+                e.preventDefault()
+                this.open = !this.open
+            }
+        })
+
+        this.navBarBackgroundControl()
+    }
+
+    searchInputActive() {
+        this.searchWrapElement.className = ["search", this.open ? " active" : ""].join("")
+    }
+
+    navBarBackgroundControl() {
+        this.navBarBackground.style.opacity = `${this.opacity}`
+    }
+}
+
+const search = new Search({
+    searchWrapElement: document.querySelector(".search") as HTMLDivElement,
+    navBarBackground: document.querySelector(".navbar-background") as HTMLDivElement,
+    backdrop: document.querySelector(".search-backdrop") as HTMLDivElement,
+})
--- wwwroot/js/search.js
+++ wwwroot/js/search.js
@@ -1,2 +1,54 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+class Search {
+    constructor(elems) {
+        this.open = false;
+        this.opacity = 0;
+        const { searchWrapElement, navBarBackground, backdrop } = elems;
+        this.searchWrapElement = searchWrapElement;
+        this.navBarBackground = navBarBackground;
+        this.backdrop = backdrop;
+        this.setupEventListener();
+    }
+    setupEventListener() {
+        this.searchWrapElement.addEventListener("click", (e) => {
+            e.stopPropagation();
+            if (!this.open) {
+                this.open = true;
+            }
+            this.searchInputActive();
+        });
+        this.backdrop.addEventListener("click", (e) => {
+            e.stopPropagation();
+            if (this.open) {
+                this.open = false;
+            }
+            this.searchInputActive();
+        });
+        window.addEventListener("scroll", () => {
+            if (window.scrollY <= 56) {
+                this.opacity = 0.0178571 * window.scrollY;
+            }
+            else {
+                this.opacity = 1;
+            }
+        });
+        window.addEventListener("keydown", (e) => {
+            if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+                e.preventDefault();
+                this.open = !this.open;
+            }
+        });
+        this.navBarBackgroundControl();
+    }
+    searchInputActive() {
+        this.searchWrapElement.className = ["search", this.open ? " active" : ""].join("");
+    }
+    navBarBackgroundControl() {
+        this.navBarBackground.style.opacity = `${this.opacity}`;
+    }
+}
+const search = new Search({
+    searchWrapElement: document.querySelector(".search"),
+    navBarBackground: document.querySelector(".navbar-background"),
+    backdrop: document.querySelector(".search-backdrop"),
+});
