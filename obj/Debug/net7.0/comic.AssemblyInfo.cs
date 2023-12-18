2c3ca6c292b5ae16b10ff8ebcfff55cb7bbf8fa5
pphatt <vutienphat0809@gmail.com>
2023-12-19 01:56:14 +0700

    feat: add product detail can open user panel

--- obj/Debug/net7.0/comic.AssemblyInfo.cs
+++ obj/Debug/net7.0/comic.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("comic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f22c2dce188d51af940bbe45ede4f39b067945e")]
 [assembly: System.Reflection.AssemblyProductAttribute("comic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("comic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
